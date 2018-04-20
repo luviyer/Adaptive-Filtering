aaed9cce3bfa2fd5c0af40bea0524f1698ceb6ae
Luv Iyer <luviyer@gmail.com>
2018-04-20 17:41:27 -0400

    Commit to be overwritten...pre Darsh

--- Part_A.m
+++ Part_A.m
@@ -1,10 +1,10 @@
-f_signal = 0.3;
-f_noise = 0.8;
+f_signal = 0.5*pi;
+f_noise = 0.3;
 
 N = 50000;
 n = (0:N-1)';
 s = cos(2*pi*f_signal*n);
-w = 760*cos(2*pi*f_noise*n);
+w = 1000*cos(2*pi*f_noise*n);
 
 x = s+w;
 
@@ -18,17 +18,22 @@ for k=10:N-1
     y(k) = x(k) + a(k)*x(k-1) + x(k-2) - r*a(k)*y(k-1)-r*r*y(k-2);
     a(k+1) = a(k)- mu*y(k)*x(k-1);
     
-    if abs(a(k+1)) > 2
+    if abs(a(k+1)) >= 2
         a(k+1) = 0;
     end 
 end
 
+a_converged = a(N); % The value of a we should use since algorithm converges
+b = [1 a_converged 1];
+a_coeff = [1 r*a_converged r*r];
+[h,w] = freqz(b, a_coeff, 'whole', 2001);
 figure(1)
-stem(n(500:510), x(500:510))
-title('x')
+h=fftshift(h);
+w=w-pi;
+plot(w/pi, 20*log10(abs(h)))
 
 figure(2)
-stem(n(500:510), y(500:510))
+stem(n(N-100:N), y(N-100:N))
 title('y')
 figure(3)
 plot(n, a)
