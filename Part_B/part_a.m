23da542ebbf47ef1d64c94f4f62b5903083efd5c
Luv Iyer <luviyer@gmail.com>
2018-04-23 10:16:37 -0400

    Changed amplitude of Part a signal to +/- 1

--- Part_B/part_a.m
+++ Part_B/part_a.m
@@ -2,18 +2,18 @@ clc; clear;
 % Initialization of constants
 L = 4;
 M = 20;
-N = 10000;
-mu = 1E-6;
-SNR = 25;
+N = 1000;
+mu = .01;
+SNR = 35;
 filter_delay = 5;
 w = linspace(-pi,pi,N);
 
 % Initialization of all relevant signals
-s = randi([0,1],1,N)*200-100;
+s = randi([0,1],1,N)*2-1;
 h = [0.3,1,0.7,0.3,0.2];
 x = zeros(1,N); y = zeros(1,N); error = zeros(1,N);
 h_filter = zeros(1, M+1);
-h_filter(M/2)=0.5;
+% h_filter(M/2)=1;
 
 % Determine output sequence x[n] for input into FIR adaptive equalizer by
 % performing the convolution of s and h.
@@ -31,7 +31,7 @@ x = awgn(x,SNR);
 
 
 % Determine output sequence y[n] for output of FIR adaptive equalizer
-for n = 21:N
+for n = 40:N
     for m = 1:M+1
             y(n) = y(n) + x(n-m+1)*h_filter(m);
     end
